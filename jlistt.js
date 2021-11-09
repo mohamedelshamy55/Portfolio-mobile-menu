8db980e438ef953968077ace1896a5bc71857640
mohamedelshamy55 <39459774+mohamedelshamy55@users.noreply.github.com>
2021-11-09 22:37:58 +0200

    Update jlistt.js

--- jlistt.js
+++ jlistt.js
@@ -13,37 +13,37 @@ const navMenu = document.querySelector('header > nav > .hamburger');
  * @description handles the opening and closing of the hamburger mobile menu
  */
 export const clickHandler = (e) => {
-    e.preventDefault();
-    navMenu.insertAdjacentHTML(
-        'beforeend',
-        `
-    <div class="hamburger-mobile-menu ">
-        <div class="close">
-          <img src="closeiconw.svg" />
-        </div>
-        <ul class="ulList">
-          <li><a href="#projects">Portfolio</a></li>
-          <li><a href="#about">About</a></li>
-          <li><a href="#contact">Contact</a></li>
-        </ul>
-      </div>
-    `,
-    );
-    const menuList = document.querySelector('.hamburger-mobile-menu');
-    helperFunc('blur(15px)', menuList, 'none', 0);
-    const closer = document.querySelector('.close');
-    closer.addEventListener('click', () => helperFunc('blur(0)', menuList, 'block', '770px'));
-    const ulList = Array.from(document.querySelectorAll('.ulList li'));
+  e.preventDefault();
+  navMenu.insertAdjacentHTML(
+    'beforeend',
+    `
+  <div class="hamburger-mobile-menu ">
+    <div class="close">
+      <img src="closeiconw.svg" />
+    </div>
+    <ul class="ulList">
+      <li><a href="#projects">Portfolio</a></li>
+      <li><a href="#about">About</a></li>
+      <li><a href="#contact">Contact</a></li>
+    </ul>
+    </div>
+  `,
+  );
+const menuList = document.querySelector('.hamburger-mobile-menu');
+helperFunc('blur(15px)', menuList, 'none', 0);
+const closer = document.querySelector('.close');
+  closer.addEventListener('click', () => helperFunc('blur(0)', menuList, 'block', '770px'));
+  const ulList = Array.from(document.querySelectorAll('.ulList li'));
 
-    ulList.forEach((list) => {
-        list.addEventListener('click', () => {
-            helperFunc('blur(0)', menuList, 'block', '770px');
-        });
-    });
+  ulList.forEach((list) => {
+    list.addEventListener('click', () => {
+    helperFunc('blur(0)', menuList, 'block', '770px');
+  });
+  });
 };
 
 // nav list active toggle
 export const activeHandler = (e) => {
-    document.querySelector('.nav-list.active').classList.remove('active');
-    e.currentTarget.classList.add('active');
+  document.querySelector('.nav-list.active').classList.remove('active');
+  e.currentTarget.classList.add('active');
 };
