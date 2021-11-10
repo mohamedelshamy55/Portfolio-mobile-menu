ebf4275acea40e459720eb48f1aa167d6c197661
mohamedelshamy55 <39459774+mohamedelshamy55@users.noreply.github.com>
2021-11-11 00:28:16 +0200

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,4 +1,4 @@
-import cmod from './cmod.js';
+import cardModel from './cmod.js';
 import { findItem } from './querysel.js';
 
 /**
@@ -8,7 +8,7 @@ import { findItem } from './querysel.js';
  * @description {handles model popup}
  */
 export default function modelHandler(e) {
-  const card = findItem(e.currentTarget, cmod); // find the item in the model
+  const card = findItem(e.currentTarget, cardModel); // find the item in the model
   const {
     imgSrc,
     header,
@@ -27,42 +27,42 @@ export default function modelHandler(e) {
   <div class="card model-card">
   <div class="card-details">
   <header>
-    <h2>${header}</h2>
-    <img src="closeiconb.svg" alt="close icon" id="closeImg"/>
+  <h2>${header}</h2>
+  <img src="closeiconb.svg" alt="close icon" id="closeImg"/>
   </header>
   <div class="author-lang">
-    <span>${authorLang}</span>
-    <ul>
-      <li>${authorList1}</li>
-      <li>${authorList2}</li>
-    </ul>
+  <span>${authorLang}</span>
+  <ul>
+    <li>${authorList1}</li>
+    <li>${authorList2}</li>
+  </ul>
   </div>
   <figure>
-    <img
-      src=${imgSrc}
-      alt="project image"
-    />
+  <img
+    src=${imgSrc}
+    alt="project image"
+  />
   </figure>
   <div class="card-flex">
   <p class="project-desc">${para}</p>
   <div class="contain">
   <ul class="project-lang">
-    <li>${langList1}</li>
-    <li>${langList2}</li>
-    <li>${langList3}</li>
-    <li class="optional">${langList4}</li>
+  <li>${langList1}</li>
+  <li>${langList2}</li>
+  <li>${langList3}</li>
+  <li class="optional">${langList4}</li>
   </ul>
   <hr />
   <div class="action-btn">
-    <button class="project-btn" type="button">
-      <a href="https://mohamedelshamy55.github.io/Portfolio-mobile-menu/" target="_blank">See live <img src="liveicon.svg" /></a>
-    </button>
-    <button class="project-btn" type="button">
-      <a href="https://mohamedelshamy55.github.io/Portfolio-mobile-menu/" target="_blank">See Source <img src="giticon.svg" /></a>
-    </button>
-    </div>
-    </div>
-    </div>
+  <button class="project-btn" type="button">
+  <a href="https://mohamedelshamy55.github.io/Portfolio-mobile-menu/" target="_blank">See live <img src="liveicon.svg" /></a>
+  </button>
+  <button class="project-btn" type="button">
+    <a href="https://mohamedelshamy55.github.io/Portfolio-mobile-menu/" target="_blank">See Source <img src="giticon.svg" /></a>
+  </button>
+  </div>
+  </div>
+  </div>
   </div>
   </div>
 `;
