e1ee4ebd5a69331643c1fba3e42305755b1f9515
mohamedelshamy55 <39459774+mohamedelshamy55@users.noreply.github.com>
2021-11-11 00:39:45 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 import {
-    clickHandler,
-    hamburger,
-    navList,
-    activeHandler,
-    btns,
+  clickHandler,
+  hamburger,
+  navList,
+  activeHandler,
+  btns,
 } from './jlistt.js';
 
 import modelHandler from './popup.js';
 
 import {
-    inputs,
-    formHandler,
-    form,
-    blurHandler,
+  inputs,
+  formHandler,
+  form,
+  blurHandler,
 } from './querysel.js';
 
 // hamburger toggle
@@ -20,14 +20,14 @@ hamburger.addEventListener('click', clickHandler);
 
 // nav list active toggle
 navList.forEach((item) => {
-    item.addEventListener('click', activeHandler);
+  item.addEventListener('click', activeHandler);
 });
 
 // handle button click for model popup
 btns.forEach((btn) => btn.addEventListener('click', modelHandler));
 
 inputs.forEach((input) => {
-    input.addEventListener('blur', blurHandler);
+  input.addEventListener('blur', blurHandler);
 });
 
 form.addEventListener('submit', formHandler);
